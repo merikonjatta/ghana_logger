c35d6169a9209d22bfb445bd22f2ad8ad5241261
Shinya Maeyama <merikonjatta@gmail.com>
2014-03-15 10:47:59 +0900

    Debug test

--- test/test_ghana_logger.rb
+++ test/test_ghana_logger.rb
@@ -1,4 +1,6 @@
-require 'helper'
+puts RUBY_VERSION
+puts ENV['GEM_HOME']
+require File.expand_path('../helper', __FILE__)
 
 class TestGhanaLogger < Test::Unit::TestCase
   should "probably rename this file and start testing for real" do
